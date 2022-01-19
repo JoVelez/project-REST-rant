9ba8617d99d8532fb32345c8b962fcfda29e4aef
Joleena Velez <joleenavelez@gmail.com>
2022-01-18 20:15:11 -0800

    Added Bootstrap

--- views/home.jsx
+++ views/home.jsx
@@ -7,6 +7,7 @@ function home () {
           <main>
               <h1>HOME</h1>
               <a href="/places">
+                <img src="/images/foodpic1.jpg" alt="Isk what food this is" height="500" width="600"></img>
   <button className="btn-primary">Places Page</button>
 </a>
           </main>
