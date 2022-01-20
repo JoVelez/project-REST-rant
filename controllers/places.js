af45c749866f8bbf55b1a154d707daea267be79d
Joleena Velez <joleenavelez@gmail.com>
2022-01-19 16:02:08 -0800

    render new view

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,9 @@
 const router = require('express').Router()
 
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
 router.get('/', (req, res) => {
    
     let places = [{
@@ -18,5 +22,6 @@ router.get('/', (req, res) => {
 
     res.render('places/index', {places})
 })
+
 
 module.exports = router
