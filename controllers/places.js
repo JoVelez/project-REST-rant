39752debfc738e0b5bb831960e75fd563bce64bc
Joleena Velez <joleenavelez@gmail.com>
2022-01-19 17:05:45 -0800

    pic error

--- controllers/places.js
+++ controllers/places.js
@@ -1,32 +1,28 @@
-const router = require('express').Router()
+const router = require('express').Router();
+const places = require('../models/places');
 
 router.get('/new', (req, res) => {
-  res.render('places/new')
+  res.render('places/new');
 })
 
 router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('POST /places')
+  console.log(req.body);
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body);
+  res.redirect('/places');
 })
 
 router.get('/', (req, res) => {
-
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-      }]
-
-    res.render('places/index', {places})
+    res.render('places/index', { places });
 })
-
 
 module.exports = router
