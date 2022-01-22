4f3696dd1a29385bc14a7de36b722dfdde72a486
Joleena Velez <joleenavelez@gmail.com>
2022-01-21 19:03:04 -0800

    Fixed Pic Issue

--- controllers/places.js
+++ controllers/places.js
@@ -1,42 +1,10 @@
 const router = require('express').Router();
 const places = require('../models/places');
 
-//GET /places
-router.get('/', (req, res) => {
-  res.render('places/index', { places });
-})
 
 //NEW
 router.get('/new', (req, res) => {
   res.render('places/new');
-})
-
-//EDIT 
-router.get('/:id/edit', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-    res.render('places/edit', { place: places[id], id })
-  }
-})
-
-//SHOW
-router.get('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    res.render('places/show', { place: places[id], id })
-  }
 })
 
 //CREATE
@@ -55,6 +23,38 @@ router.post('/', (req, res) => {
   res.redirect('/places');
 })
 
+//GET /places
+router.get('/', (req, res) => {
+  res.render('places/index', { places });
+})
+
+//SHOW
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id], id })
+  }
+})
+
+//EDIT 
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id], id })
+  }
+})
 
 //UPDATE
 router.put('/:id', (req, res) => {
